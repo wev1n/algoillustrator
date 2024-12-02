e1bef797bdae1fc2f117a9f1c46affc9dc9438a0
wevin <kevin.warme@gmail.com>
2024-12-02 18:40:58 +0100

    feat: sidebar for visualizers ui added

--- src/app/_components/sidebar/app-sidebar.tsx
+++ src/app/_components/sidebar/app-sidebar.tsx
@@ -1,23 +1,10 @@
 "use client";
 
 import * as React from "react";
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react";
 
-import { NavMain } from "~/app/_components/sidebar/nav-main";
-import { NavProjects } from "~/app/_components/sidebar/nav-projects";
-import { NavUser } from "~/app/_components/sidebar/nav-user";
-import { TeamSwitcher } from "~/app/_components/sidebar/team-switcher";
+import { SidebarAlgorithms } from "~/app/_components/sidebar/sidebar-algorithms";
+import { SidebarDataStructures } from "~/app/_components/sidebar/sidebar-data-structures";
+import { NavUser } from "~/app/_components/sidebar/sidebar-user";
 import {
   Sidebar,
   SidebarContent,
@@ -25,149 +12,33 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "~/app/_components/ui/sidebar";
-
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-};
+import { sidebarData } from "./sidebar-data";
+import { Command } from "lucide-react";
+import Link from "next/link";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <Link
+          href="/"
+          className="flex gap-3 rounded-sm hover:bg-sidebar-accent"
+        >
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <Command className="size-4" />
+          </div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-semibold">AlgoIllustrator</span>
+            <span className="truncate text-xs">Hobby</span>
+          </div>
+        </Link>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <SidebarAlgorithms items={sidebarData.algorithms} />
+        <SidebarDataStructures items={sidebarData.dataStructures} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser user={sidebarData.user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
---
+++ src/app/_components/sidebar/sidebar-data.ts
@@ -0,0 +1,330 @@
+import {
+  ArrowDownAZ,
+  ChartNoAxesColumnIncreasing,
+  Command,
+  Divide,
+  Ellipsis,
+  Hash,
+  Layers,
+  List,
+  Repeat,
+  Search,
+  Text,
+  Zap,
+  Network,
+} from "lucide-react";
+
+export const sidebarData = {
+  user: {
+    name: "johndoe",
+    email: "john.doe@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "AlgoIllustrator",
+      logo: Command,
+      plan: "Hobby",
+    },
+  ],
+  algorithms: [
+    {
+      title: "Sorting",
+      url: "#",
+      icon: ArrowDownAZ,
+      isActive: true,
+      items: [
+        {
+          title: "Bubble sort",
+          url: "#",
+        },
+        {
+          title: "Selection sort",
+          url: "#",
+        },
+        {
+          title: "Insertion sort",
+          url: "#",
+        },
+        {
+          title: "Merge sort",
+          url: "#",
+        },
+        {
+          title: "Quick sort",
+          url: "#",
+        },
+        {
+          title: "Heap sort",
+          url: "#",
+        },
+        {
+          title: "Radix sort",
+          url: "#",
+        },
+        {
+          title: "Counting sort",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+      items: [
+        {
+          title: "Linear search",
+          url: "#",
+        },
+        {
+          title: "Binary search",
+          url: "#",
+        },
+        {
+          title: "Interpolation search",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "String",
+      url: "#",
+      icon: Text,
+      items: [
+        {
+          title: "Pattern matching",
+          url: "#",
+        },
+        {
+          title: "Longest common subsequence(LCS)",
+          url: "#",
+        },
+        {
+          title: "Longest palindromic subsequence(LPS)",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Graph",
+      url: "#",
+      icon: ChartNoAxesColumnIncreasing,
+      items: [
+        {
+          title: "Shortest path",
+          url: "#",
+        },
+        {
+          title: "Minimum spanning tree",
+          url: "#",
+        },
+        {
+          title: "Topological sort",
+          url: "#",
+        },
+        {
+          title: "Strongly connected components(Tarjan's algorithm)",
+          url: "#",
+        },
+        {
+          title: "A* pathfinding",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Dynamic programming",
+      url: "#",
+      icon: Repeat,
+      items: [
+        {
+          title: "Fibonacci sequence",
+          url: "#",
+        },
+        {
+          title: "0/1 Knapsack problem",
+          url: "#",
+        },
+        {
+          title: "Longest increasing subsequence(LIS)",
+          url: "#",
+        },
+        {
+          title: "Edit distance",
+          url: "#",
+        },
+        {
+          title: "Matrix chain multiplication",
+          url: "#",
+        },
+        {
+          title: "Subset sum problem",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Divide and conquer",
+      url: "#",
+      icon: Divide,
+      items: [
+        {
+          title: "Binary search",
+          url: "#",
+        },
+        {
+          title: "Merge sort",
+          url: "#",
+        },
+        {
+          title: "Quick sort",
+          url: "#",
+        },
+        {
+          title: "Closest pair of points",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Greedy algorithms",
+      url: "#",
+      icon: Zap,
+      items: [
+        {
+          title: "Activity selection",
+          url: "#",
+        },
+        {
+          title: "Huffman coding",
+          url: "#",
+        },
+        {
+          title: "Fractional knapsack",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      url: "#",
+      icon: Ellipsis,
+      items: [
+        {
+          title: "Bit manipulation",
+          url: "#",
+        },
+        {
+          title: "Fast exponentiation",
+          url: "#",
+        },
+        {
+          title: "GCD using Euclid's algorithm",
+          url: "#",
+        },
+        {
+          title: "Reservoir sampling",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  dataStructures: [
+    {
+      title: "Linear",
+      url: "#",
+      icon: List,
+
+      items: [
+        {
+          title: "Array",
+          url: "#",
+        },
+        {
+          title: "Linked list",
+          url: "#",
+        },
+        {
+          title: "Stack",
+          url: "#",
+        },
+        {
+          title: "Queue",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Hierarchical",
+      url: "#",
+      icon: Network,
+      items: [
+        {
+          title: "Binary tree",
+          url: "#",
+        },
+        {
+          title: "Binary search tree",
+          url: "#",
+        },
+        {
+          title: "Heap",
+          url: "#",
+        },
+        {
+          title: "Trie",
+          url: "#",
+        },
+        {
+          title: "Graph",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Hashing",
+      url: "#",
+      icon: Hash,
+
+      items: [
+        {
+          title: "Hash table with open addressing or separate chaining",
+          url: "#",
+        },
+        {
+          title: "Collision resolution techniques",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Advanced",
+      url: "#",
+      icon: Layers,
+
+      items: [
+        {
+          title: "Segment tree",
+          url: "#",
+        },
+        {
+          title: "Fenwick tree",
+          url: "#",
+        },
+        {
+          title: "Disjoint set union",
+          url: "#",
+        },
+        {
+          title: "Red-black tree",
+          url: "#",
+        },
+        {
+          title: "Suffix array",
+          url: "#",
+        },
+      ],
+    },
+  ],
+};
