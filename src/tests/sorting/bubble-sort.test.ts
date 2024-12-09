52a9dc1d46370d333c0abef7d5b3975464f81064
wevin <kevin.warme@gmail.com>
2024-12-09 19:37:59 +0100

    refactor: updated filepath and describe name on bubble-sort.test.ts

--- src/tests/sorting/bubble-sort.test.ts
+++ src/tests/sorting/bubble-sort.test.ts
@@ -3,7 +3,7 @@ import { bubbleSort } from "../../algorithms/bubble-sort";
 
 const array = [9, 2, 6, 8, 3, 5, 1, 7, 4];
 
-describe("Bubble Sort Test", () => {
+describe("Bubble Sort", () => {
   it("Should sort the array in ascending order", () => {
     const sortedArray = bubbleSort(array);
     expect(sortedArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
